d88cdb47aae1cf4886f59e514bc78791dd470cea
SirusCodes <hey@darshanrander.com>
2023-02-17 18:53:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+import 'src/app.dart';
+import 'src/service_locator.dart';
+
+Future<void> main() async {
+  usePathUrlStrategy();
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black.withOpacity(0.002),
+    ),
+  );
+  runApp(const PaisaApp());
+}
---
+++ lib/src/presentation/settings/pages/setting_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import '../../../data/settings/authenticate.dart';
+import '../../widgets/future_resolve.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../core/common.dart';
+import '../../../core/enum/box_types.dart';
+import '../../../service_locator.dart';
+import '../widgets/biometrics_auth_widget.dart';
+import '../widgets/currency_change_widget.dart';
+import '../widgets/setting_option.dart';
+import '../widgets/settings_color_picker_widget.dart';
+import '../widgets/settings_group_card.dart';
+import '../widgets/version_widget.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: context.materialYouAppBar(
+        context.loc.settingsLabel,
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          SettingsGroup(
+            title: context.loc.colorsLabel,
+            options: [
+              SettingsColorPickerWidget(
+                settings: locator.get(
+                  instanceName: BoxType.settings.name,
+                ),
+              ),
+            ],
+          ),
+          SettingsGroup(
+            title: context.loc.othersLabel,
+            options: [
+              const CurrencyChangeWidget(),
+              const Divider(),
+              FutureResolve<Authenticate>(
+                future: locator.getAsync<Authenticate>(),
+                builder: (value) => FutureResolve<bool>(
+                  future: value.auth.isDeviceSupported(),
+                  builder: (supported) {
+                    return Visibility(
+                      visible: supported,
+                      child: Column(
+                        children: [
+                          BiometricAuthWidget(
+                            authenticate: locator.get<Authenticate>(),
+                          ),
+                          const Divider(),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SettingsOption(
+                title: context.loc.backupAndRestoreLabel,
+                subtitle: context.loc.backupAndRestoreDescLabel,
+                onTap: () {
+                  ScaffoldMessenger.maybeOf(context)?.showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Disabled',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onPrimaryContainer,
+                            ),
+                      ),
+                      backgroundColor:
+                          Theme.of(context).colorScheme.primaryContainer,
+                      elevation: 6,
+                      behavior: SnackBarBehavior.floating,
+                    ),
+                  );
+                  //GoRouter.of(context).pushNamed(exportAndImport);
+                },
+              ),
+            ],
+          ),
+          SettingsGroup(
+            title: context.loc.socialLinksLabel,
+            options: [
+              SettingsOption(
+                title: context.loc.appRateLabel,
+                subtitle: context.loc.appRateDescLabel,
+                onTap: () => launchUrl(
+                  Uri.parse(playStoreUrl),
+                  mode: LaunchMode.externalApplication,
+                ),
+              ),
+              const Divider(),
+              SettingsOption(
+                title: context.loc.githubLabel,
+                subtitle: context.loc.githubTextLabel,
+                onTap: () => launchUrl(
+                  Uri.parse(gitHubUrl),
+                  mode: LaunchMode.externalApplication,
+                ),
+              ),
+              const Divider(),
+              SettingsOption(
+                title: context.loc.telegramLabel,
+                subtitle: context.loc.telegramGroupLabel,
+                onTap: () => launchUrl(
+                  Uri.parse(telegramGroupUrl),
+                  mode: LaunchMode.externalApplication,
+                ),
+              ),
+              const Divider(),
+              SettingsOption(
+                title: context.loc.privacyPolicyLabel,
+                onTap: () => launchUrl(
+                  Uri.parse(termsAndConditionsUrl),
+                  mode: LaunchMode.externalApplication,
+                ),
+              ),
+              const Divider(),
+              const VersionWidget(),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(context.loc.madeWithLoveInIndiaLabel),
+          ),
+        ],
+      ),
+    );
+  }
+}
