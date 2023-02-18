6fad36821ae21861b1cde30414ebc706bce2fe3a
SirusCodes <hey@darshanrander.com>
2023-02-18 07:59:47 +0530

    add reminder notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 import 'src/app.dart';
 import 'src/service_locator.dart';
@@ -7,6 +10,7 @@ import 'src/service_locator.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
+  await scheduleNotifications();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
@@ -14,4 +18,33 @@ Future<void> main() async {
     ),
   );
   runApp(const PaisaApp());
+}
+
+Future<void> scheduleNotifications() async {
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Asia/Kolkata'));
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin()
+        ..initialize(const InitializationSettings(
+          android: AndroidInitializationSettings('app_icon'),
+        ));
+  flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.requestPermission();
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    0,
+    'Add Expenses',
+    'Add your expenses for the day!!',
+    tz.TZDateTime.now(tz.local).add(const Duration(minutes: 1)),
+    const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'your channel id',
+        'your channel name',
+        channelDescription: 'your channel description',
+      ),
+    ),
+    uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime,
+  );
 }
