f3dc3356ebe7a665dc4747edd12ecadeb29f02e5
SirusCodes <hey@darshanrander.com>
2023-02-18 08:07:09 +0530

    rename and login bug

--- lib/src/presentation/login/pages/user_name_page.dart
+++ lib/src/presentation/login/pages/user_name_page.dart
@@ -78,151 +78,155 @@ class _UserNamePageState extends State<UserNamePage> {
             child: SafeArea(
               child: FractionallySizedBox(
                 widthFactor: 0.8,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 16),
-                    ColorFiltered(
-                      colorFilter: ColorFilter.mode(
-                        Theme.of(context).colorScheme.primary,
-                        BlendMode.srcIn,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 16),
+                      ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          Theme.of(context).colorScheme.primary,
+                          BlendMode.srcIn,
+                        ),
+                        child: const Icon(
+                          Icons.wallet,
+                          size: 72,
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.wallet,
-                        size: 72,
+                      const SizedBox(height: 16),
+                      RichText(
+                        text: TextSpan(
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).colorScheme.onSurface,
+                                letterSpacing: 0.8,
+                              ),
+                          text: context.loc.welcomeLabel,
+                          children: [
+                            TextSpan(
+                              text: ' ${context.loc.appTitle}',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    RichText(
-                      text: TextSpan(
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall
-                            ?.copyWith(
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).colorScheme.onSurface,
-                              letterSpacing: 0.8,
-                            ),
-                        text: context.loc.welcomeLabel,
-                        children: [
-                          TextSpan(
-                            text: ' ${context.loc.appTitle}',
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          )
-                        ],
+                      const SizedBox(height: 6),
+                      Text(
+                        context.loc.welcomeDescLabel,
+                        style:
+                            Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurface
+                                      .withOpacity(0.75),
+                                  letterSpacing: 0.6,
+                                ),
                       ),
-                    ),
-                    const SizedBox(height: 6),
-                    Text(
-                      context.loc.welcomeDescLabel,
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onSurface
-                                .withOpacity(0.75),
-                            letterSpacing: 0.6,
-                          ),
-                    ),
-                    const SizedBox(height: 16),
-                    Form(
-                      key: _formState,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextFormField(
-                            controller: _nameController,
-                            decoration: InputDecoration(
-                              hintText: context.loc.enterNameLabel,
-                              label: Text(context.loc.nameLabel),
+                      const SizedBox(height: 16),
+                      Form(
+                        key: _formState,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            TextFormField(
+                              controller: _nameController,
+                              decoration: InputDecoration(
+                                hintText: context.loc.enterNameLabel,
+                                label: Text(context.loc.nameLabel),
+                              ),
+                              keyboardType: TextInputType.name,
+                              validator: (val) {
+                                if (val!.isNotEmpty) {
+                                  return null;
+                                } else {
+                                  return context.loc.enterNameLabel;
+                                }
+                              },
                             ),
-                            keyboardType: TextInputType.name,
-                            validator: (val) {
-                              if (val!.isNotEmpty) {
-                                return null;
-                              } else {
-                                return context.loc.enterNameLabel;
-                              }
-                            },
-                          ),
-                          const SizedBox(height: 16),
-                          const Text("Matrial Status"),
-                          _matrialStatusWidget(),
-                          const SizedBox(height: 16),
-                          const Text("Gender"),
-                          _genderWidget(),
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            controller: _ageController,
-                            decoration: const InputDecoration(
-                              hintText: "Enter your age",
-                              label: Text("Age"),
-                            ),
-                            keyboardType: TextInputType.number,
-                            validator: (val) {
-                              if (val == null || val.isEmpty) {
-                                return "Enter your age";
-                              }
+                            const SizedBox(height: 16),
+                            const Text("Matrial Status"),
+                            _matrialStatusWidget(),
+                            const SizedBox(height: 16),
+                            const Text("Gender"),
+                            _genderWidget(),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              controller: _ageController,
+                              decoration: const InputDecoration(
+                                hintText: "Enter your age",
+                                label: Text("Age"),
+                              ),
+                              keyboardType: TextInputType.number,
+                              validator: (val) {
+                                if (val == null || val.isEmpty) {
+                                  return "Enter your age";
+                                }
 
-                              return null;
-                            },
-                          ),
-                          const SizedBox(height: 16),
-                          _educationWidget(),
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            controller: _capGainController,
-                            decoration: const InputDecoration(
-                              hintText: "Enter Capital Gains",
-                              suffixText: "₹",
-                              label: Text("Capital Gains"),
-                            ),
-                            keyboardType: TextInputType.number,
-                            validator: (val) {
-                              if (val!.isNotEmpty) {
                                 return null;
-                              }
-                              return "Enter capital gains";
-                            },
-                          ),
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            controller: _capLossController,
-                            decoration: const InputDecoration(
-                              hintText: "Enter Capital Loss",
-                              suffixText: "₹",
-                              label: Text("Capital Loss"),
+                              },
                             ),
-                            keyboardType: TextInputType.number,
-                            validator: (val) {
-                              if (val!.isNotEmpty) {
-                                return null;
-                              }
-                              return "Enter Capital Loss";
-                            },
-                          ),
-                          const SizedBox(height: 16),
-                          TextFormField(
-                            controller: _incomeController,
-                            decoration: const InputDecoration(
-                              hintText: "Enter Income per year",
-                              suffixText: "₹",
-                              label: Text("Income per year"),
+                            const SizedBox(height: 16),
+                            _educationWidget(),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              controller: _capGainController,
+                              decoration: const InputDecoration(
+                                hintText: "Enter Capital Gains",
+                                prefixText: "₹",
+                                label: Text("Capital Gains"),
+                              ),
+                              keyboardType: TextInputType.number,
+                              validator: (val) {
+                                if (val!.isNotEmpty) {
+                                  return null;
+                                }
+                                return "Enter capital gains";
+                              },
                             ),
-                            keyboardType: TextInputType.number,
-                            validator: (val) {
-                              if (val!.isNotEmpty) {
-                                return null;
-                              }
-                              return "Enter Income per year";
-                            },
-                          ),
-                        ],
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              controller: _capLossController,
+                              decoration: const InputDecoration(
+                                hintText: "Enter Capital Loss",
+                                prefixText: "₹",
+                                label: Text("Capital Loss"),
+                              ),
+                              keyboardType: TextInputType.number,
+                              validator: (val) {
+                                if (val!.isNotEmpty) {
+                                  return null;
+                                }
+                                return "Enter Capital Loss";
+                              },
+                            ),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              controller: _incomeController,
+                              decoration: const InputDecoration(
+                                hintText: "Enter Income per year",
+                                prefixText: "₹",
+                                label: Text("Income per year"),
+                              ),
+                              keyboardType: TextInputType.number,
+                              validator: (val) {
+                                if (val!.isNotEmpty) {
+                                  return null;
+                                }
+                                return "Enter Income per year";
+                              },
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 80),
+                    ],
+                  ),
                 ),
               ),
             ),
